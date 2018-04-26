2cd20851f7d9137f92d045a11d7f87f96e12c9b8
Newelln-NJRS340 <impersonatr@gmail.com>
2018-04-25 23:43:57 -0700

    Update the Test Suites

--- projects/newelln/Assn2/Calendar/src/test/java/calendar/ApptTest.java
+++ projects/newelln/Assn2/Calendar/src/test/java/calendar/ApptTest.java
@@ -7,21 +7,50 @@ import static org.junit.Assert.*;
 import calendar.Appt;
 import calendar.CalendarUtil;
 public class ApptTest  {
-
-  @Test(timeout = 4000)
-  public void test00()  throws Throwable  {
-      Appt appt0 = new Appt(15, 30, 9, 14, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
-      String string0 = appt0.toString();
-      assertEquals(2, appt0.getRecurBy());
-      assertFalse(appt0.isRecurring());
-      assertEquals("\t14/9/2018 at 3:30pm ,Birthday Party, This is my birthday party\n", string0);
-      assertEquals(0, appt0.getRecurIncrement());
-      appt0.setValid();
-  }
-@Test(timeout = 4000)
- public void test01()  throws Throwable  {
-
-
-}
+	@Test(timeout = 4000)
+	public void test00()  throws Throwable  {
+		Appt appt0 = new Appt(15, 30, 9, 14, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+		String string0 = appt0.toString();
+		assertEquals(2, appt0.getRecurBy());
+		assertEquals(15, appt0.getStartHour());
+		assertEquals(30, appt0.getStartMinute());
+		
+		assertFalse(appt0.isRecurring());
+		assertEquals("\t14/9/2018 at 3:30pm ,Birthday Party, This is my birthday party\n", string0);
+		assertEquals(0, appt0.getRecurIncrement());
+		appt0.setValid();
+	}
+	
+	@Test(timeout = 4000)
+	public void test01()  throws Throwable  {
+		Appt appt1 = new Appt(9, 12, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+		String string1 = appt1.toString();
+		assertEquals(9, appt1.getStartDay());
+		assertEquals(12, appt1.getStartMonth());
+		assertEquals(2018, appt1.getStartYear());
+		
+		assertEquals("Birthday Party", appt1.getTitle());
+		assertEquals("This is my birthday party", appt1.getDescription());
+		assertEquals("xyz@gmail.com", appt1.getEmailAddress());
+		appt1.setValid();
+	}
+	
+	@Test(timeout = 4000)
+	public void test02()  throws Throwable  {
+		Appt appt2 = new Appt(15, 65, 9, 12, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+		appt2.setValid();
+	}
+	
+	@Test(timeout = 4000)
+	public void test03()  throws Throwable  {
+		Appt appt3 = new Appt(15, 30, 40, 12, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+		appt3.setValid();
+	}
+	
+	@Test(timeout = 4000)
+	public void test04()  throws Throwable  {
+		Appt appt4 = new Appt(12, 12, 9, 9, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+		appt4.setValid();
+	}
 
 }
--- projects/newelln/Assn2/Calendar/src/test/java/calendar/CalDayTest.java
+++ projects/newelln/Assn2/Calendar/src/test/java/calendar/CalDayTest.java
@@ -6,17 +6,53 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import calendar.Appt;
 import calendar.CalDay;
+import java.util.GregorianCalendar; //add this???
 
 
 public class CalDayTest{
-
-  @Test(timeout = 4000)
-  public void test00()  throws Throwable  {
-
-  }
-  @Test(timeout = 4000)
-  public void test01()  throws Throwable  {
-
-  }
-
+	
+	@Test(timeout = 4000)
+	public void test00()  throws Throwable  {
+		CalDay cal0 = new CalDay();
+		assertFalse(cal0.isValid());
+	}
+	
+	@Test(timeout = 4000)
+	public void test01()  throws Throwable  {
+		GregorianCalendar greg = new GregorianCalendar(2018, 9, 9);
+		CalDay cal1 = new CalDay(greg);
+		assertTrue(cal1.isValid());
+		String string1 = cal1.getFullInfomrationApp(cal1);
+		
+	}
+	
+	@Test(timeout = 4000)
+	public void test02()  throws Throwable  {
+		GregorianCalendar greg = new GregorianCalendar(2018, 9, 9);
+		CalDay cal2 = new CalDay(greg);
+		Appt appt2 = new Appt(12, 12, 9, 9, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+		cal2.addAppt(appt2);
+	}
+	
+	@Test(timeout = 4000)
+	public void test03()  throws Throwable  {
+		GregorianCalendar greg = new GregorianCalendar(2018, 9, 9);
+		CalDay cal3 = new CalDay(greg);
+		Appt appt3 = new Appt(12, 12, 14, 14, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+		cal3.addAppt(appt3);
+		
+	}
+	
+	@Test(timeout = 4000)
+	public void test04()  throws Throwable  {
+		GregorianCalendar greg = new GregorianCalendar(2018, 9, 9);
+		CalDay cal4 = new CalDay(greg);
+		Appt appt1 = new Appt(12, 12, 9, 9, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+		Appt appt3 = new Appt(15, 12, 9, 9, 2018, "Birthday Party", "This is my birthday party", "xyz@gmail.com");
+		cal4.addAppt(appt1);
+		cal4.addAppt(appt3);
+		String string4 = cal4.getFullInfomrationApp(cal4);
+		String string5 = cal4.toString();
+	}
+	
 }
