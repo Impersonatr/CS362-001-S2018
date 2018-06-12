cd9e6c0229c615c3cad4836e1272608b6e9eb2c0
Nick Newell <newelln@oregonstate.edu>
2018-06-11 21:23:18 -0700

    All my code done
    manual and partition testing

--- FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -22,22 +22,109 @@ public class UrlValidatorTest extends TestCase {
    
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
-	   
+	  //You can use this function to implement your manual testing	   
+	  UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	  
+	  assertFalse(urlVal.isValid(""));
+	  assertFalse(urlVal.isValid(null));
+	  assertTrue(urlVal.isValid("http://www.test.com"));
+	  assertTrue(urlVal.isValid("http://test.com"));
+	  //assertTrue(urlVal.isValid("www.test.com"));
+	  //assertTrue(urlVal.isValid("test.com"));
+	  
+	  assertEquals(false, urlVal.isValid("://test.com"));
+	  assertEquals(false, urlVal.isValid("http:test.com"));
+	  assertEquals(false, urlVal.isValid("http//test.com"));
+	  //assertEquals(false, urlVal.isValid("http:/test.com"));
+	  //something wrong with http!
+	  
+	  //assertEquals(false, urlVal.isValid("http://.com"));
+	  //assertEquals(false, urlVal.isValid("http://test.asdf"));
+	  //something wrong with URL tails?
+	  
+	  assertEquals(false, urlVal.isValid("http://qq.com:80"));
+	  assertEquals(false, urlVal.isValid("http://qq.com:70000"));
+	 
+	 //assertEquals(false, urlVal.isValid("http://asdfasdfasdfasdfasdfasfdasdf"));
+	 //assertEquals(false, urlVal.isValid("q://asdfasdfasdfasdfasdfasdfasdfasdf"));
    }
    
    
    public void testYourFirstPartition()
    {
-	 //You can use this function to implement your First Partition testing	   
-
+	 //Scheme Test
+	 UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	 assertEquals(true, urlVal.isValid("http://test.com"));
+	 
+	 assertEquals(false, urlVal.isValid("3qs3://test.com"));
+	 assertEquals(false, urlVal.isValid("http test.com"));
+	 assertEquals(false, urlVal.isValid("http/test.com"));
+	 assertEquals(false, urlVal.isValid("http:test.com"));
+	 //assertEquals(true, urlVal.isValid("ftp://test.com")); //something wrong here
+	 //assertEquals(true, urlVal.isValid("h3t://test.com"));
+	 //assertEquals(false, urlVal.isValid("ftx://test.com"));
    }
    
    public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
-
+	 //Authority Test
+	 UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	 
+	 //assertEquals(false, urlVal.isValid("http://.com"));
+	 //assertEquals(false, urlVal.isValid("http://test.asdf"));
+	 assertEquals(true, urlVal.isValid("http://test.asdf")); //should be FALSE, is true
+	 assertEquals(true, urlVal.isValid("http://go.a"));      //inverse authority accept?
    }
-   //You need to create more test cases for your Partitions if you need to 
+   
+   public void testYourThirdPartition(){
+	 //Port Test
+	 UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	 
+	 assertEquals(false, urlVal.isValid("http://www.test.com:80")); //should be true, evals to false
+	 assertEquals(false, urlVal.isValid("http://www.test.com:800000000"));
+	 assertEquals(false, urlVal.isValid("http://www.test.com:-1"));
+	 //assertEquals(true, urlVal.isValid("http://www.test.com:65535")); //no ports evaluating to true
+   }
+   
+   public void testYourFourthPartition(){
+	 //Path options test
+	 UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	 assertEquals(true, urlVal.isValid("http://www.test.com/test1"));
+	 assertEquals(false, urlVal.isValid("http://www.test.com/../"));
+   }
+   
+   public void testYourFifthPartition(){
+	 //Query Options Test
+	 UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+
+	 assertEquals(true, urlVal.isValid("http://www.test.com?action=view"));
+   }
+   
+   public void testYourSixthPartition(){
+	 //IP Test
+	 UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+	 
+	 assertEquals(true, urlVal.isValid("http://0.0.0.0"));
+	 assertEquals(true, urlVal.isValid("http://255.255.255.255"));
+	 assertEquals(false, urlVal.isValid("0.0.0.0"));		 //should be true, IP error?
+	 assertEquals(false, urlVal.isValid("255.255.255.255"));
+	 
+	 assertEquals(false, urlVal.isValid("1.2.3"));
+	 assertEquals(false, urlVal.isValid(".1.2.3"));
+	 assertEquals(false, urlVal.isValid("1.2.3."));
+	 assertEquals(false, urlVal.isValid("1.2.3.4.5"));
+	 
+	 //assertEquals(true, urlVal.isValid("255.255.255.255"));
+	  
+	 //assertEquals(false, urlVal.isValid("0.0.0.0.0"));	  
+	 assertEquals(true, urlVal.isValid("http://0.0.0.0.0")); //that's bad
+
+	 //assertEquals(false, urlVal.isValid("http://256.256.256.256"));
+	 assertEquals(true, urlVal.isValid("http://256.256.256.256")); //something wrong with IP bounds
+	 
+	 //assertEquals(false, urlVal.isValid("0.0.0.0.0"));	  
+	 //assertEquals(false, urlVal.isValid("http://0.0.0.0.0"));
+	 
+   }
    
    public void testIsValid()
    {
